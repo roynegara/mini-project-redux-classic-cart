395048a382cd2e2d50b6d4cc711da701f80ea012
roynegara <roybayunegara92@gmail.com>
2024-03-22 10:38:30 +0700

    add Modal with onClick={(e) => e.stopPropagation()}

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,19 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { clearCart } from '../features/cart/cartSlice'
+import { closeModal } from '../features/modal/modalSlice'
+
 const Modal = () => {
+  const dispatch = useDispatch()
   return (
-    <aside className="modal-container">
-      <div className="modal">
+    <aside className="modal-container" onClick={() => {dispatch(closeModal())}}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
         <h4>remove all items from your shopping cart?</h4>
         <div className="btn-container">
-          <button type="button" className="btn confirm-btn">
+          <button type="button" className="btn confirm-btn" onClick={() => { dispatch(clearCart()); dispatch(closeModal()) }}>
             confirm
           </button>
-          <button type="button" className="btn clear-btn">
+          <button type="button" className="btn clear-btn" onClick={() => { dispatch(closeModal())}}>
             cancel
           </button>
         </div>
